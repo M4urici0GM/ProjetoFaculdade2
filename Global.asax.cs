6e13c4f4d0fce833ad34adee2a183aee09a267f9
Mauricio Barbosa <mauricio.coelho@etec.sp.gov.br>
2019-11-26 00:36:17 -0300

    Initial Commit

---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace ProjetoFaculdade2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void RegisterRoutes(RouteCollection routes)
+        {
+            FriendlyUrlSettings friendlyUrlSettings = new FriendlyUrlSettings();
+            friendlyUrlSettings.AutoRedirectMode = RedirectMode.Off;
+            
+            routes.MapPageRoute("home", "", "");
+            routes.EnableFriendlyUrls(friendlyUrlSettings);
+            
+        }
+    }
+}
